9631ee2abce78ba7e5b9573a8798a0c7eccc3059
AndrejLomnicky <lomnicky.andrej@gmail.com>
2026-02-04 09:29:12 +0100

    commit

---
+++ File_shredder.py
@@ -0,0 +1,49 @@
+#subory txt,   /-normalne lomitko, \-obratene lomitko
+fr = open("subory/data.txt","r",encoding="utf-8")
+#citacia hlava sa nastavila na zaciatok
+
+
+
+# prvý spôsob:
+#for row in fr:
+#   print(row, end="")
+
+
+# druhý spôsob:
+#jozo=fr.readlines()
+# vytvorim list ktoreho prvkami sú celé riadky
+#print(jozo)
+
+
+
+#Tretí spôsob:
+#first_line = fr.readline()
+#print(first_line)
+#for row in fr:
+#    print(row)
+#words = 0
+#words_no_e = 0
+#words_e = 0
+#for row in fr:
+#row = row.strip() #odrbal som posledny enter
+    #words += 1
+    #if "e" in row:
+        #words_e += 1
+#print(words)
+#print(words_e)
+#print(words_no_e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
